b241d0027647d84e126c1b379bbd09c0a0b0e666
Admin <admin@Admins-MacBook-Air.local>
2024-06-30 11:17:44 -0400

    Assignment 1 Completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,52 +11,74 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? " , 
-                  "True or false: 5 kilometer == 5000 meters? " , 
-                  "(5 + 3)/2 * 10 = ? " , 
-                  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , 
-                  "What is the minimum crew size for the ISS? " ];
+let questions = ["Who was the first American woman in space? ", 
+                  "True or false: 5 kilometer == 5000 meters? ", 
+                  "(5 + 3)/2 * 10 = ? ", 
+                  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+                  "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-let candidateInput = [];
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Enter your name: ");
+  candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  //candidateAnswer = input.question(question);
-for (let i=0; i <= 4; i++) {
-  candidateInput = input.question(questions[i]);
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer // 
+
+let i = 0;
+while (i <= 4) {
+  let candidateInput = '';
+    candidateInput = input.question(`${i + 1}) ${questions[i]}`);
+    candidateAnswers.push(candidateInput);
+    i++;
 }
-  candidateAnswers.push(candidateInput); //worked without this and without the extra variable "candidateInput"
+  return candidateAnswers
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-//if (correctAnswer === candidateAnswer) {console.log("Correct!");} else {console.log("Incorrect")}
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-if (correctAnswers === candidateAnswers) {
-  console.log(`You got all answers correct. Here were your answers: ${candidateAnswers} \n Here are the correct answers: ${correctAnswers}`);
-} else {
-  console.log(`Sorry, that was incorrect. These are the correct answers: ${correctAnswers}`);
-} //this is giving the opposite output from what's expected.
+for (let i = 0; i < candidateAnswers.length; i++) {
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+  } else {
+    console.log(`Wrong! The correct answer is: ${correctAnswers[i]}`);
+  }
+}
 
+//TODO 3.2 use this variable to calculate the candidates score.
 
-  return grade;
+let numOfCorrectAnswers = 0;
+
+for(let i = 0; i < correctAnswers.length; i++) {
+ if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+  numOfCorrectAnswers++;
+ }
+}
+
+let numOfQuestions = correctAnswers.length;
+let grade = (numOfCorrectAnswers / numOfQuestions) * 100;  
+console.log(`>>> Overall Grade: ${grade}% (${numOfCorrectAnswers} of ${numOfQuestions} responses correct) <<<`);
+  if (grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`)
+  } else {
+    console.log(`>>> Status: FAILED <<<`)
+  }
+return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hi " + candidateName + "!");
+   console.log(`Candidate name: ${candidateName}`);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz(this.candidateAnswers); //will likely need a last console.log with the output that is expected//
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
